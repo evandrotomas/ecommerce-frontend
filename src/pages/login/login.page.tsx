993829dd4f7001bb15498be6438c7b6d53c4cd23
Evandro Tomas <evandrotomas@hotmail.com>
2025-11-11 19:01:07 -0300

    feat: add initial Login Oage and Login Route

---
+++ src/pages/login/login.page.tsx
@@ -0,0 +1,35 @@
+import Header from '../../components/header/header.component'
+import {
+  LoginContainer,
+  LoginContent,
+  LoginHeadline,
+  LoginInputContainer,
+  LoginSubtitle
+} from './login.styles'
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header />
+
+      <LoginContainer>
+        <LoginContent>
+          <LoginHeadline>Entre com a sua conta</LoginHeadline>
+
+          {/* Button */}
+
+          <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
+
+          <LoginInputContainer>
+            {/* E-mail Input */}
+            {/* Password Input */}
+          </LoginInputContainer>
+
+          {/* Button */}
+        </LoginContent>
+      </LoginContainer>
+    </>
+  )
+}
+
+export default LoginPage

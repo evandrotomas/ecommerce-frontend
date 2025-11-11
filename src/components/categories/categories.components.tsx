51b60e041e2e9ec219ac5eb9932c802479d7120a
Evandro Tomas <evandrotomas@hotmail.com>
2025-11-11 16:18:08 -0300

    refactor: add Styled Components to Categories Component

--- src/components/categories/categories.components.tsx
+++ src/components/categories/categories.components.tsx
@@ -7,9 +7,9 @@ import CategoryItem from '../category-item/category-item.component'
 import Category from '../../types/category.types'
 
 // Styles
-import './categories.styles.css'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../../config/firebase.config'
+import { CategoriesContainer, CategoriesContent } from './categories.styles'
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([])
@@ -35,15 +35,15 @@ const Categories = () => {
   }, [])
 
   return (
-    <div className='categories-container'>
-      <div className='categories-content'>
+    <CategoriesContainer>
+      <CategoriesContent>
         {categories.map((category) => (
           <div key={category.id}>
             <CategoryItem category={category} />
           </div>
         ))}
-      </div>
-    </div>
+      </CategoriesContent>
+    </CategoriesContainer>
   )
 }
 

8392569e030d590995eefa9b8b61c66d79e2b7f8
Evandro Tomas <evandrotomas@hotmail.com>
2025-11-11 21:49:27 -0300

    feat: add Input Error Message Component

---
+++ src/components/input-error-message/input-error-message.component.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react'
+
+// Styles
+import { InputErrorMessageContainer } from './input-error-message.styles'
+
+interface InputErrorMessageProps {
+  children: ReactNode
+}
+
+const InputErrorMessage = ({ children }: InputErrorMessageProps) => {
+  return <InputErrorMessageContainer>{children}</InputErrorMessageContainer>
+}
+
+export default InputErrorMessage
---
+++ src/components/input-error-message/input-error-message.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import Colors from '../../theme/theme.colors'
+
+export const InputErrorMessageContainer = styled.p`
+  width: 100%;
+  color: ${Colors.error};
+  margin-top: 5px;
+`

37431cf82f3c68c2fd273dc46a293396f1ac1e70
Evandro Tomas <evandrotomas@hotmail.com>
2025-11-13 16:21:53 -0300

    feat: add UserContext

---
+++ src/contexts/user.context.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { createContext, useState, ReactNode } from 'react'
+import User from '../types/user.types'
+
+interface UserContextProps {
+  children: ReactNode
+}
+
+interface IUserContext {
+  currentUser: User | null
+  isAuthenticated: boolean
+  loginUser: (user: User) => void
+  logoutUser: () => void
+}
+
+export const UserContext = createContext<IUserContext>({
+  currentUser: null,
+  isAuthenticated: false,
+  loginUser: () => {},
+  logoutUser: () => {}
+})
+
+const UserContextProvider = ({ children }: UserContextProps) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+  const isAuthenticated = currentUser != null
+
+  const loginUser = (user: User) => {
+    setCurrentUser(user)
+  }
+
+  const logoutUser = () => {
+    setCurrentUser(null)
+  }
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser, isAuthenticated, loginUser, logoutUser }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export default UserContextProvider

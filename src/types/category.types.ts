491105fc20b31523ac0120128346ec619aa6e56c
Evandro Tomas <evandrotomas@hotmail.com>
2025-11-14 19:10:54 -0300

    feat: add product type on category type

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -1,9 +1,11 @@
+import Product from './product.types'
+
 interface Category {
   id: string
   name: string
   displayName: string
   imageUrl: string
-  products: string
+  products: Product[]
 }
 
 export default Category

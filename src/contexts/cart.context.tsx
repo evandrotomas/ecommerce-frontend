ff79bb77eff480195c996e4dcbf585b6685ff1d9
Evandro Tomas <evandrotomas@hotmail.com>
2025-11-17 11:55:31 -0300

    feat: add Cart Context

---
+++ src/contexts/cart.context.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { createContext, FunctionComponent, useState } from 'react'
+import CartProduct from '../types/cart.typs'
+
+interface ICartContext {
+  isVisible: boolean
+  products: CartProduct[]
+  toggleCart: () => void
+}
+
+const CartContext = createContext<ICartContext>({
+  isVisible: false,
+  products: [],
+  toggleCart: () => {}
+})
+
+const CartContextProvider: FunctionComponent<{ children: React.ReactNode }> = ({
+  children
+}) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [products] = useState<CartProduct[]>([])
+
+  const toggleCart = () => {
+    setIsVisible((prevState) => !prevState)
+  }
+
+  return (
+    <CartContext.Provider value={{ isVisible, products, toggleCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartContextProvider

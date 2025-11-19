5e01addbd52625620a2b5ffa58ea289489262517
Evandro Tomas <evandrotomas@hotmail.com>
2025-11-19 14:18:35 -0300

    feat: ad  stripe api integation to checkout

--- src/components/checkout/checkout.component.tsx
+++ src/components/checkout/checkout.component.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useContext } from 'react'
+import { FunctionComponent, useContext, useState } from 'react'
 import { CartContext } from '../../contexts/cart.context'
 import { BsBagCheck } from 'react-icons/bs'
 
@@ -13,12 +13,34 @@ import {
 // Components
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
+import axios from 'axios'
+import Loading from '../loading/loading.component'
 
 const Checkout: FunctionComponent = () => {
   const { products, productsTotalPrice } = useContext(CartContext)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFinishPurchaseClick = async () => {
+    try {
+      setIsLoading(true)
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL!}/create-checkout-session`,
+        {
+          products
+        }
+      )
+
+      window.location.href = data.url
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <CheckoutContainer>
+      {isLoading && <Loading />}
       <CheckoutTitle>Checkout</CheckoutTitle>
 
       {products.length > 0 ? (
@@ -30,7 +52,10 @@ const Checkout: FunctionComponent = () => {
           </CheckoutProducts>
 
           <CheckoutTotal>Total R${productsTotalPrice}</CheckoutTotal>
-          <CustomButton startIcon={<BsBagCheck size={20} />}>
+          <CustomButton
+            startIcon={<BsBagCheck size={20} />}
+            onClick={handleFinishPurchaseClick}
+          >
             Finalizar Compra
           </CustomButton>
         </>
